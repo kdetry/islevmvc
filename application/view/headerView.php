83dc63bd51b8348a41eb62932391acce3af91060
LENOVO-PC\Mustafa <mustafaislev@outlook.com.tr>
2016-09-04 21:59:02 +0300

    commit

---
+++ application/view/headerView.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $meta_title; ?></title>
+        <meta name="description" content="<?php echo $meta_description; ?>">
+    </head>
+    <body>
---
+++ application/view/homeView.php
@@ -0,0 +1,2 @@
+<?php echo $header; ?>
+<?php echo $content; ?>
