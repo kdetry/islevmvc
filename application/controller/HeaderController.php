2be668684c26f96d06f0373a3153c53c5f8f3ffd
LENOVO-PC\Mustafa <mustafaislev@outlook.com.tr>
2016-09-04 21:57:53 +0300

    commit

---
+++ application/controller/HeaderController.php
@@ -0,0 +1,18 @@
+<?php
+
+class HeaderController extends BaseController
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getHeader(){
+        $document = new Document;
+        $data['meta_title'] = $document->getTitle();
+        $data['meta_description'] = $document->getDescription();
+
+        $response = new Response();
+        $header = $response->view('headerView.php', $data);
+        return $header;
+    }
+}
---
+++ application/controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+class HomeController extends BaseController
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index(){
+        $document = new Document();
+        $document->setTitle('Hello Title');
+        $document->setDescription('This is Hello description');
+
+        $headerController = new HeaderController();
+        $header = $headerController->getHeader();
+
+        $data = array(
+            'header'    =>  $header,
+            'content'   =>  'Hello HomeController !'
+        );
+
+        $response = new Response();
+        $response->setOutput($response->view('homeView.php', $data));
+        $response->output();
+    }
+}
